ad42bf84a5e9ab622be176406f4db3faaff86af1
gomezhyuuga <gomezhyuuga@gmail.com>
2013-09-15 22:21:04 -0500

    Agregado jquery

--- eventos/views.py
+++ eventos/views.py
@@ -1,5 +1,6 @@
 from eventos.models import Evento
 from django.shortcuts import render
+from django.core.mail import send_mail // Para enviar mails
 
 def index(request):
 	template_name = "eventos/index.html"
@@ -8,3 +9,12 @@ def index(request):
 	context = { "eventos_list": lista }
 
 	return render(request, template_name, context)
+
+def send_contact_email(request):
+	nombre = request.POST.get("nombre")
+	email = request.POST.get("email")
+	asunto = request.POST.get("asunto")
+	mensaje = request.POST.get("mensaje")
+	# Enviar mail
+	# send_mail(asunto, mensaje, email, recipient_list, fail_silently=False,
+		# auth_user=None, auth_password=None, connection=None, html_message=None)
